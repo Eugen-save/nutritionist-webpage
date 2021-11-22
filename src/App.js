7c07eb71ee290788afd1c3e1730b174093b01159
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-11-22 06:48:32 +0100

    Components added to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { About } from "./About";
+import { Backdrop } from "./Backdrop";
+import { MiddleBackdrop } from "./MiddleBackdrop";
+import { Navigation } from "./Navigation";
+import { Services } from "./Services";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export const App = () => {
+    return (
+        <>
+            <Navigation />
+            <Backdrop />
+            <About />
+            <MiddleBackdrop />
+            <Services />
+        </>
+    )
 }
-
-export default App;
