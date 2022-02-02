4f251878a2cfb377802c4d8018d8ce76794ce5d5
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2022-02-02 17:28:55 +0100

    background color changed to gradient animation

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,4 +1,16 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle, keyframes } from "styled-components";
+
+const gradient = keyframes`
+        0% {
+        background-position: 0% 50%;
+      }
+      50% {
+        background-position: 100% 50%;
+      }
+      100% {
+        background-position: 0% 50%;
+      }
+`;
 
 export const GlobalStyle = createGlobalStyle`
 
@@ -12,11 +24,13 @@ html {
 
 body {
     font-family: 'Roboto', sans-serif;
-    background: ${({ theme }) => theme.colors.mercury};
-    color: ${({theme}) => theme.colors.woodsmoke};
+    background: linear-gradient(270deg,#84fab0,#8fd3f4,#00a86b);
+    color: ${({ theme }) => theme.colors.woodsmoke};
     margin: 0 auto;
     font-style: normal;
     font-weight: normal;
+    background-size: 180% 180%;
+    animation: ${gradient} 9s ease infinite;
 }
 
 button {
