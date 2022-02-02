7e984f5dfe8fb01d2e1854997926cfb322ca2768
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2022-02-02 17:29:53 +0100

    text added to component

--- src/MiddleBackdrop/index.js
+++ src/MiddleBackdrop/index.js
@@ -1,11 +1,32 @@
-import { BlackRectangle, MiddleHeader, MiddlePoster } from "./styled";
+import { 
+    BlackRectangle, 
+    Button, 
+    List, 
+    ListItem, 
+    MiddleHeader, 
+    MiddlePoster, 
+    SubHeader, 
+    WrapperInfo 
+} from "./styled";
 
 export const MiddleBackdrop = () => (
     <MiddlePoster>
         <BlackRectangle>
+            <WrapperInfo>
             <MiddleHeader>
                 Почему стоит обратиться ко мне?
             </MiddleHeader>
+            <SubHeader>Я помогу Вам</SubHeader>
+            <List>
+                <ListItem>нормализовать вес</ListItem>
+                <ListItem>улучшить настроение и общее самочувствие</ListItem>
+                <ListItem>увеличить работоспособность и концентрацию внимания</ListItem>
+                <ListItem>повысить стрессоустойчивость, иммунитет</ListItem>
+            </List>
+            <Button>
+                Записаться на консультацию
+            </Button>
+            </WrapperInfo>
         </BlackRectangle>
     </MiddlePoster>
 )
--- src/MiddleBackdrop/styled.js
+++ src/MiddleBackdrop/styled.js
@@ -5,13 +5,13 @@ export const MiddlePoster = styled.div`
     background-image: url("${poster}");
     background-repeat: no-repeat;
     background-size: cover;
-    height: 797px;
-    margin-top: 120px;
+    height: 950px;
+    margin-top: 80px;
 `;
 
 export const BlackRectangle = styled.div`
     background-color: ${({theme}) => theme.colors.woodsmokeLight};
-    height: 797px;
+    height: 950px;
     width: 100%;
     display: flex;
     justify-content: center;
@@ -24,5 +24,36 @@ export const MiddleHeader = styled.h2`
     letter-spacing: 0.05em;
     color: ${({theme}) => theme.colors.white};
     height: 100px;
-    align-self: center;
+`;
+
+export const SubHeader = styled.h3`
+    font-weight: normal;
+    font-size: 48px;
+    color: ${({theme}) => theme.colors.white};
+`;
+
+export const WrapperInfo = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+export const List = styled.ul`
+    color: ${({theme}) => theme.colors.white};
+    font-size: 32px;
+`;
+
+export const ListItem = styled.li`
+    padding: 10px;
+`;
+
+export const Button = styled.button`
+    padding: 20px;
+    margin: 20px;
+    border-radius: 15px;
+    border: none;
+    font-size: 32px;
+    color: ${({theme}) => theme.colors.white};
+    background-color: ${({theme}) => theme.colors.jade};
 `;
