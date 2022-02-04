91700f858ed6e9ea0f942e3471a0d5a594ad07fe
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2022-02-04 07:30:42 +0100

    new colors added to theme

--- src/theme.js
+++ src/theme.js
@@ -4,6 +4,10 @@ export const theme = {
         woodsmoke: "#18181B",
         white: "#ffffff",
         woodsmokeLight: "rgba(24, 24, 27, 0.5)",
-        jade: "#00A86B"
+        jade: "#00A86B",
+        curiousBlue: "#229ED9",
+        azureRadiance: "#1877F2",
+        rose: "#E60073",
+        cinnabar: "#EA4335"
     }
 }
