de192ef030362a1751d161512d6d2b60430acc18
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2022-02-11 16:14:02 +0100

    packdrop and middle backdrop removed from App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import { About } from "./About";
-import { Backdrop } from "./Backdrop";
 import { Contacts } from "./Contacts";
 import { MiddleBackdrop } from "./MiddleBackdrop";
 import { Navigation } from "./Navigation";
-import { ToggleButton } from "./Navigation/ToggleButton";
 import { ScrollButton } from "./ScrollButton";
 import { Services } from "./Services";
 
@@ -12,12 +9,9 @@ import { Services } from "./Services";
 export const App = () => {
 
     return (
-        <>
-        { navToggled ? <Navigation handleNavToggle={handleNavToggle} /> : null }
+        <>  
             <Navigation />
-            <Backdrop />
             <About />
-            <MiddleBackdrop />
             <Services />
             <Contacts />
             <ScrollButton />
